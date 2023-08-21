329a9c9e65859a7a2bdfd6690290aa2263cd7e24
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:37:51 +0300

    check querySelector

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 'use strict';
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent='🎉Correct Number';
 document.querySelector('.number').textContent='13';
 document.querySelector('.score').textContent='10';
 document.querySelector('.guess').textContent='10';
 console.log(document.querySelector('.guess').value)
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+
+});

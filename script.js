306bfbf1fc486c74728246cead1ca473986a5e1c
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:32:06 +0300

    guess change

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent='🎉Correct Number';
 document.querySelector('.number').textContent='13';
 document.querySelector('.score').textContent='10';
+document.querySelector('.guess').textContent='10';
+console.log(document.querySelector('.guess').value)

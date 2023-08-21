06b1aa1e1a1107e2e5df367cd2ce31cc1073da7a
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-22 01:00:54 +0300

    updating highscore

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.querySelector('.score').textContent='10';
 document.querySelector('.guess').textContent='10';
 console.log(document.querySelector('.guess').value)
 */
-const secretNumber= Math.trunc(Math.random()*20)+1;
+let secretNumber= Math.trunc(Math.random()*20)+1;
 let score=20;
+let highscore=0;
 document.querySelector('.number').textContent=secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -20,8 +21,14 @@ document.querySelector('.check').addEventListener('click', function () {
   p//playes wins
   }else if(guess===secretNumber){
     document.querySelector('.message').textContent='🎉 Correct Number !';
+    document.querySelector('.number').textContent=secretNumber;
+
     document.querySelector('body').style.backgroundColor='#60b347';
     document.querySelector('.number').style.width='30rem';
+    if(score>highscore){
+      highscore=score;
+      document.querySelector('.highscore').textContent=highscore;
+    }
   }
   //guess is too high
   else if(guess>secretNumber){
@@ -44,10 +51,22 @@ document.querySelector('.check').addEventListener('click', function () {
       }else{
         document.querySelector('.message').textContent='💥 You lost the game !';
         document.querySelector('.score').textContent=0;
+      }
+      }
+});
+document.querySelector('.again').addEventListener('click',function () {
+  score=20;
+  secretNumber= Math.trunc(Math.random()*20)+1;
+  document.querySelector('.message').textContent='Start guesing...';
+  document.querySelector('.score').textContent=score;
+  document.querySelector('.number').textContent='?';
+  document.querySelector('.guess').value='';
+  document.querySelector('body').style.backgroundColor='#222';
+  document.querySelector('.number').style.width='15rem';
+
   
-    
-      }
-      }
+
+
 
 
 });

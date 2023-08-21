da0794a4f6d13e579f57305c97a543185e953cf0
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:53:03 +0300

    number between 1-20

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.querySelector('.score').textContent='10';
 document.querySelector('.guess').textContent='10';
 console.log(document.querySelector('.guess').value)
 */
-const number= Math.random();
+const number= Math.trunc(Math.random()*20)+1;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess= Number(document.querySelector('.guess').value);

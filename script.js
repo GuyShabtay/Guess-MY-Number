3f4e43acbcaba6844849aeaebbb05b9d648fc63b
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:42:28 +0300

    guess

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.querySelector('.guess').textContent='10';
 console.log(document.querySelector('.guess').value)
 */
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess= document.querySelector('.guess').value;
+  console.log(guess);
 
 });

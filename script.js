9e314faa95ee8620f5d7639cd66cc74be62f024e
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:44:00 +0300

    guess as a number

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.querySelector('.guess').textContent='10';
 console.log(document.querySelector('.guess').value)
 */
 document.querySelector('.check').addEventListener('click', function () {
-  const guess= document.querySelector('.guess').value;
+  const guess= Number(document.querySelector('.guess').value);
   console.log(guess);
 
 });

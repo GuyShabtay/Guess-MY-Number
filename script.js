6b81b342a7b03dcec23cad6ddd66b3834862e3be
Guy Shabtay <guysh16@ac.sce.ac.il>
2023-08-21 23:46:39 +0300

    no input check

--- script.js
+++ script.js
@@ -10,5 +10,10 @@ console.log(document.querySelector('.guess').value)
 document.querySelector('.check').addEventListener('click', function () {
   const guess= Number(document.querySelector('.guess').value);
   console.log(guess);
+  if(!guess){
+    document.querySelector('.message').textContent='⛔ No Number !';
+
+
+  }
 
 });
